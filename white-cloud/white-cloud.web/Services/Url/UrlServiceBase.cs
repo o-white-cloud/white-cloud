c3d10b0781c253beb90337a20d277b117aad886a
o-white-cloud <office@white-cloud.ro>
2022-05-28 17:32:06 +0300

    Added missing file

---
+++ white-cloud/white-cloud.web/Services/Url/UrlServiceBase.cs
@@ -0,0 +1,21 @@
+﻿namespace white_cloud.web.Services
+{
+    public class UrlServiceBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UrlServiceBase(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string BuildUrl(string path, object? queryParams = null)
+        {
+            var scheme = _httpContextAccessor.HttpContext?.Request.Scheme;
+            var host = _httpContextAccessor.HttpContext?.Request.Host.Value;
+
+            var query = queryParams == null ? "" : "?" + string.Join('&', queryParams.GetType().GetProperties().Select(p => $"{p.Name}={p.GetValue(queryParams)}"));
+            return $"{scheme}://{host}/{path.Trim('/')}{query}";
+        }
+    }
+}
